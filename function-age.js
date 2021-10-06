77894a71e0956d149f9d2f9e7f156d5e06139529
sahnasanar <sahnasamad@gmail.com>
2021-10-06 19:14:43 -0400

    file added

---
+++ function-age.js
@@ -0,0 +1,7 @@
+
+function ageCalculator(name,yearOfBirth,currentYear){
+  var age = currentYear-yearOfBirth
+return name + " is " + age + " years old";
+}
+
+console.log(ageCalculator("Ferdinand", 1988, 2015));
